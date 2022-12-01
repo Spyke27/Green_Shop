6f40e25c89fbae22c057affc5dd40dce7a368779
Spyke27 <rodrigoeufrasio2@gmail.com>
2022-12-01 13:02:39 -0300

    cadastroUsuario

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './componentes/estaticos/footer/Footer';
 import Navbar from './componentes/estaticos/navbar/Navbar';
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 
 function App() {
   return (
     <>
-    <Navbar />
+    <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/home' element={<Home/>} />
+        </Routes>
+        <Footer/>
+      </Router>
     </>
   );
 }
